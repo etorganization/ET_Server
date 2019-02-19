a075e46a711ba264129007ef73975018690e12e5
zhangjian <zhangjian@qq.com>
2019-02-19 16:28:07 +0800

    添加excel tool

---
+++ Tools/ExcelExporter/Helper/MD5Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ETTools
+{
+	public static class MD5Helper
+	{
+		
+		public static string FileMD5(string filePath)
+		{
+			byte[] retVal;
+            using (FileStream file = new FileStream(filePath, FileMode.Open))
+			{
+				MD5 md5 = new MD5CryptoServiceProvider();
+				retVal = md5.ComputeHash(file);
+			}
+			return retVal.ToHex("x2");
+		}
+	}
+}
